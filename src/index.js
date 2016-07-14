9f6ee86cf9b08cfd70a93fe9eb7fb683bd1b845f
Deepak Kapoor <kapoordeepak@gmail.com>
2016-07-14 14:13:11 +1000

    prepare for deployment

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import './styles/styles.css';
 //import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+require('file?name=[name].[ext]!./index.html');
 
 render(
   <Router history={browserHistory} routes={routes} />,
---
+++ webpack.config.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /\.jsx$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
