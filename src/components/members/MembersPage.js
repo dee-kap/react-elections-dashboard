35d2eedf666e7dd925dd8f95c352061fd2b91963
Deepak Kapoor <kapoordeepak@gmail.com>
2016-07-14 17:23:23 +1000

    Use react-bootstrap-table

--- src/components/members/MembersPage.js
+++ src/components/members/MembersPage.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import string from 'string';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 export class MembersPage extends Component {
 
@@ -6,46 +8,56 @@ export class MembersPage extends Component {
     super();
     this.state = {members: []};
     this.getMembers = this.getMembers.bind(this);
+    this.onSearchTextChange = this.onSearchTextChange.bind(this);
+    this.nameFormatter = this.nameFormatter.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMembers();
   }
 
   getMembers(search) {
     $.get('https://elec-960cb.firebaseio.com/housecandidates.json', (data) => {
       if(!search) {
-        this.setState({members: data.slice(0, 25)});
+        this.setState({members: data});
+      } else if(search.length > 1) {
+        search = search.toLowerCase();
+        let members = data.filter((value, index) => {
+          if(value.GivenNm.toLowerCase().startsWith(search) || value.Surname.toLowerCase().startsWith(search)) {
+            return value;
+          }
+        });
+        this.setState({members: members});
       }
     });
   }
 
-	render() {
-    this.getMembers();
-    const members = this.state.members.map(function(m, index) {
-      return (
-          <tr key={index}>
-            <td></td>
-            <td>{m.GivenNm}</td>
-            <td>{m.Surname}</td>
-            <td>{m.StateAb}</td>
-            <td>{m.PartyNm}</td>
-          </tr>
-        );
-    });
+  onSearchTextChange(event) {
+    this.getMembers(event.target.value);
+  }
 
+  nameFormatter(cell, row) {
+    return string(cell).capitalize().orig;
+
+  }
+
+	render() {
+    let options = {
+      noDataText: 'Loading data...',
+      sizePerPage: 25,
+      searchDelayTime: 700,
+      clearSearch: true
+    };
+ 
 		return (
 			<div>
-        <table className="table table-striped">
-          <thead>
-              <tr>
-                  <th>#</th>
-                  <th>First Name</th>
-                  <th>Last Name</th>
-                  <th>State</th>
-                  <th>Party</th>
-              </tr>
-          </thead>
-          <tbody>
-              {members}
-          </tbody>
-      </table>
+        
+        <BootstrapTable options={options} data={this.state.members} paginationSize="25" striped={true} hover={true} pagination={true} search={true}>
+          <TableHeaderColumn dataField="GivenNm" isKey={true} dataSort={true}>Given Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="Surname" dataFormat={this.nameFormatter} dataSort={true}>Surame</TableHeaderColumn>
+          <TableHeaderColumn dataField="StateAb" dataSort={true}>State</TableHeaderColumn>
+          <TableHeaderColumn dataField="PartyNm" dataSort={true}>Party</TableHeaderColumn>
+        </BootstrapTable>
 
       </div>
 		);
